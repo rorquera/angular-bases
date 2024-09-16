2438b870f7b93be6acab5ff1dd2b8cb3e030e031
rorquera <arckanhell1@gmail.com>
2024-09-16 09:18:06 -0500

    cambio en nivel de acceso a privado para el servicio bdzSerice

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { DbzService } from '../services/dbz.service';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: 'main-page.component.html',
 })
 export class MainPageComponent {
-  constructor(public dbzService: DbzService) {}
+  constructor(private dbzService: DbzService) {}
+
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter(id: string): void {
+    this.dbzService.onDeleteCharacter(id);
+  }
+
+  onNewCharacterPadre(character: Character): void {
+    this.dbzService.onNewCharacterPadre(character);
+  }
 }
